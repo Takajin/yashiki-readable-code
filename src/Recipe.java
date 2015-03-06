133986c1c642fef7d2f00ac93a815835aa708bc4
yashiki <m-yashiki@cnt.biglobe.co.jp>
2015-03-06 12:08:45 +0900

    仕様５

--- src/Recipe.java
+++ src/Recipe.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.*;
 
 public class Recipe {
 	public static void main(String[] args) {
@@ -11,8 +12,14 @@ public class Recipe {
 			BufferedReader bufferReader = new BufferedReader(new FileReader(fileName));
 	
 			String menu;
+			List <String> menuList = new ArrayList<>();
   			while((menu = bufferReader.readLine()) != null){
-      				System.out.println(menu);
+      				//System.out.println(menu);
+				menuList.add(menu);
+			}
+
+			for( int i = 0; i < menuList.size(); i++ ) {
+				System.out.println( i + ":" + menuList.get(i));
 			}
     		}catch(FileNotFoundException e){
 			System.out.println(e);

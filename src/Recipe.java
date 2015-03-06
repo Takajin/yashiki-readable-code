67611762d78b11964d2d408c40b47d33261841d2
yashiki <m-yashiki@cnt.biglobe.co.jp>
2015-03-06 12:23:26 +0900

    リファクタ

--- src/Recipe.java
+++ src/Recipe.java
@@ -7,24 +7,32 @@ import java.util.*;
 
 public class Recipe {
 	public static void main(String[] args) {
-		try{
-			File fileName = new File("./recipe-data.txt");
-			BufferedReader bufferReader = new BufferedReader(new FileReader(fileName));
-	
-			String menu;
-			List <String> menuList = new ArrayList<>();
-  			while((menu = bufferReader.readLine()) != null){
-      				//System.out.println(menu);
-				menuList.add(menu);
-			}
-
+			List<String> menuList = createMenuList();
 			for( int i = 0; i < menuList.size(); i++ ) {
 				System.out.println( i + ":" + menuList.get(i));
 			}
-    		}catch(FileNotFoundException e){
+
+	}
+	
+	private static List<String> createMenuList() {
+		List <String> menuList = new ArrayList<>();
+		try{
+			File fileName = new File("./recipe-data.txt");
+			BufferedReader bufferReader = new BufferedReader(new FileReader(fileName));
+
+			String menu;
+			while((menu = bufferReader.readLine()) != null){
+	                	menuList.add(menu);
+	                }
+
+    	
+		}catch(FileNotFoundException e){
 			System.out.println(e);
 		}catch(IOException e){
   			System.out.println(e);
-		}	
+		}finally{
+			return menuList;
+		}
+	
 	}			
 }

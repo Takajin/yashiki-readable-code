8f7595ee678e80a62f0212f2751987d8bb77eea6
yashiki <m-yashiki@cnt.biglobe.co.jp>
2015-03-06 11:45:32 +0900

    仕様３

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,5 +1,23 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Recipe {
-    public static void main(String[] args) {
-        System.out.println("オムライス");
-    }
+	public static void main(String[] args) {
+		try{
+			File fileName = new File("./recipe-data.txt");
+			BufferedReader bufferReader = new BufferedReader(new FileReader(fileName));
+	
+			String menu;
+  			while((menu = bufferReader.readLine()) != null){
+      				System.out.println(menu);
+			}
+    		}catch(FileNotFoundException e){
+			System.out.println(e);
+		}catch(IOException e){
+  			System.out.println(e);
+		}	
+	}			
 }
